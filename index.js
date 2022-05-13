88d78aa205e17f585685293e1a4727c8c0d45af9
Matt-J-Stewart <mjstewart1732@gmail.com>
2022-05-13 12:07:19 -0500

    removed some functions from index.js

--- index.js
+++ index.js
@@ -2,28 +2,14 @@ const express = require('express')
 app = express()
 
 var url = require('url');
-var dt = require('./date-time');
+//var dt = require('./date-time');
 
 const port = process.env.PORT || 3000
-const majorVersion = 1
-const minorVersion = 2
 
 // Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
 
 // The app.get functions below are being processed in Node.js running on the server.
-// Implement a custom About page.
-app.get('/about', (request, response) => {
-	console.log('Calling "/about" on the Node.js server.')
-	response.type('text/plain')
-	response.send('About Node.js on Azure Template.')
-})
-
-app.get('/version', (request, response) => {
-	console.log('Calling "/version" on the Node.js server.')
-	response.type('text/plain')
-	response.send('Version: '+majorVersion+'.'+minorVersion)
-})
 
 // Return the value of 2 plus 2.
 app.get('/2plus2', (request, response) => {
@@ -43,23 +29,7 @@ app.get('/add-two-integers', (request, response) => {
 	response.send(sum.toString())
 })
 
-// Template for calculating BMI using height in feet/inches and weight in pounds.
-app.get('/calculate-bmi', (request, response) => {
-	console.log('Calling "/calculate-bmi" on the Node.js server.')
-	var inputs = url.parse(request.url, true).query
-	const heightFeet = parseInt(inputs.feet)
-	const heightInches = parseInt(inputs.inches)
-	const weight = parseInt(inputs.lbs)
 
-	console.log('Height:' + heightFeet + '\'' + heightInches + '\"')
-	console.log('Weight:' + weight + ' lbs.')
-
-	// Todo: Implement unit conversions and BMI calculations.
-	// Todo: Return BMI instead of Todo message.
-
-	response.type('text/plain')
-	response.send('Todo: Implement "/calculate-bmi"')
-})
 
 // Test a variety of functions.
 app.get('/test', (request, response) => {
