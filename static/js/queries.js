7f3cae7716687fa74669c933d5ccff0447fcfc9a
Matt-J-Stewart <mjstewart1732@gmail.com>
2022-05-20 12:23:33 -0500

    One animal shows in page

--- static/js/queries.js
+++ static/js/queries.js
@@ -1,40 +1,38 @@
 const { response } = require("express");
-
-var obj1 = undefined;
 const MongoClient = require("mongodb").MongoClient;
-const CONNECTION_URL = 'mongodb+srv://matthewjstewart:Sn3akySnak3@quizcluster.5oc2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const DATABASE_NAME = 'Quiz-Capstone'
-
+const CONNECTION_URL = 'mongodb+srv://matthewjstewart:Sn3akySnak3@animalcluster.dphuu.mongodb.net/?retryWrites=true&w=majority';
+const DATABASE_NAME = 'Collectables'
 
 function getData(callback) {
     MongoClient.connect(CONNECTION_URL, async function(error,client) {
         var database = client.db(DATABASE_NAME);
-        var collection = database.collection("Quiz");
-        var cursor = collection.find({quizName: "HST01"}).limit(1);
+        var collection = database.collection("animals");
+        var cursor = collection.find({name: "Great White Shark"}).limit(1);
         var allVals = await cursor.toArray();
         var testVar = allVals[0];
         client.close();
-        console.log(client);
+        console.log(testVar);
         callback(testVar);
     });
 };
 
-function getData2(callback) {
-    MongoClient.connect(URL2, async function(error, client) {
-        // var database = client.db(DB2);
-        // var collection = database.collection("Animal");
-        // var cursor = collection.find({animalName: "Great White Shark"}).limit(1);
-        // var allVals = await cursor.toArray();
-        // var testVar = allVals[0];
-        // client.close();
-        // console.log(testVar);
-        // callback(testVar);
-        console.log(client);
-    })
-}
+const url2 = "mongodb+srv://mattStewart:Sn3akySnak3@animalcluster.r1qzy.mongodb.net/?retryWrites=true&w=majority";
+const databasename = "Coll";
 
-   
-  
+function getData2(callback) {
+    MongoClient.connect(url2, async function(error,client) {
+        var database = client.db(databasename);
+        var collection = database.collection("animal");
+        var cursor = collection.find({name: "Great White Shark"}).limit(1);
+        var allVals = await cursor.toArray();
+        var testVar = allVals[0];
+        client.close();
+        console.log(testVar);
+        callback(testVar);
+    });
+};
+
+
 
 module.exports = {
     getData: getData,
