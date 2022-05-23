6d795bdfeadb1967e8bfc39023f6b4438d1471cc
Matt-J-Stewart <mjstewart1732@gmail.com>
2022-05-23 14:13:22 -0500

    multiple time availabilities added

--- static/js/tables.js
+++ static/js/tables.js
@@ -18,6 +18,10 @@ function buildTable(data) {
                 row.appendChild(tabData);
                 
             }
+            else if(key == "timesAvailable") {
+                tabData.innerHTML = formatTime(currItem.timesAvailable);
+                row.appendChild(tabData);
+            }
             else {
                 tabData.innerHTML = currItem[key];
                 row.appendChild(tabData);
@@ -35,4 +39,51 @@ async function fetchAllAnimals() {
     then(data => {
         buildTable(data);
     })
+}
+
+//Converts strings in format of military time (1230-1300) to standard format (12:30pm-1:00pm)
+//Look into using regular expressions
+//TODO make for multiple time frames
+function formatTime(timeFrame) {
+    var timeString = "";
+    for(let i = 0; i < timeFrame.length; i++) {
+
+        let tempString = "";
+        let startTime = timeFrame[i].substring(0,4)
+        if(startTime == "0000") {
+            startTime = "12:00am";
+        }
+        else if(startTime == "1200") {
+            startTime = "12:00pm";
+        }
+        else if(startTime < 1200) {
+            startTime = (startTime.slice(0,2) + ":" + startTime.slice(2) + "am");
+        }
+        else {
+            startTime = ("0" + (startTime.slice(0,2) - 12) + ":" + startTime.slice(2) + "pm");
+        }
+        
+        tempString += startTime + "-";
+        
+        //Repeat process for ending time
+        let endTime = timeFrame[i].substring(5);
+        if(endTime == "0000") {
+            endTime = "12:00am";
+        }
+        else if(endTime == "1200") {
+            endTime = "12:00pm";
+        }
+        else if(endTime < 1200) {
+            endTime = (endTime.slice(0,2) + ":" + endTime.slice(2) + "am");
+        }
+        else {
+            endTime = ("0" + (endTime.slice(0,2) - 12) + ":" + endTime.slice(2) + "pm");
+        }
+
+        tempString += endTime;
+        timeString += tempString + ", "
+    }
+
+    timeString = timeString.slice(0,-2); //Eliminate trailing comma
+    return timeString;
 }
